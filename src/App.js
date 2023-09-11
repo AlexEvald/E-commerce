87f59c18cb943bcf921b637e6d8e66d66c88c3a2
Alexandros-Evald <alexandroslozi@gmail.com>
2023-09-11 11:48:10 +0300

    implementing routing in the webpage, introducing a navigation bar, and incorporating a few stylistic modifications.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './categories.styles.scss'
 import Home from "./routes/home/home.component";
-import {Routes,Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
 
 function App() {
 
+    const Shop = () => {
+        return (
+            <div>
+                Iam the shop page
+            </div>
+        )
+    }
+
+
   return (
       <Routes>
-        <Route path='/'  element={<Home/>}/>
+          <Route path={'/'} element={<Navigation/>}>
+            <Route index={true}  element={<Home/>}/>  {/* when the index is true when ever the parent path match then display this component also*/}
+            <Route path='/shop'  element={<Shop/>}/>
+          </Route>
       </Routes>
+
   );
 }
 
