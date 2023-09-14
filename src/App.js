27f02734b87a352282c8979eb95221ce7f43dffd
Alexandros-Evald <alexandroslozi@gmail.com>
2023-09-14 18:20:23 +0300

    Create (1) shop carts (2) product context (3) check out pop up (initial)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import Home from "./routes/home/home.component";
 import {Routes, Route} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 
 function App() {
 
-    const Shop = () => {
-        return (
-            <div>
-                Iam the shop page
-            </div>
-        )
-    }
+
 
 
   return (
--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -4,6 +4,8 @@ import {ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import './navigation.styles.scss';
 import {UserContext} from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 const Navigation = () => {
 
     const {currentUser} = useContext(UserContext);
@@ -18,10 +20,10 @@ const Navigation = () => {
                 </Link>
                 <div className={'nav-links-container'}>
                     <Link className={'nav-link'} to={'/shop'}> SHOP</Link>
-
                     {currentUser ? (<span className={'nav-link'} onClick={signOutUser}> SIGN OUT</span>) : (<Link className={'nav-link'} to={'/auth'}> SIGN IN</Link>)}
-
+                    <CartIcon/>
                 </div>
+                <CartDropdown/>
             </div>
             <Outlet/>  {/* Outlet this replaces the located component from the nested Router elements*/}
         </Fragment>
